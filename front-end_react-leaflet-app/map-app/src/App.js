bb921a4cb9914e48f677d0fc3a15af4f13eab8ba
Sam W <61138994+samwise-311@users.noreply.github.com>
2022-09-15 08:08:57 -0600

    finishing #46

--- front-end_react-leaflet-app/map-app/src/App.js
+++ front-end_react-leaflet-app/map-app/src/App.js
@@ -1,4 +1,17 @@
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+//import {Icon} from "leaflet";
+import './App.css';
+
 function App() {
-  return <div className='App'>Hello World</div>;
+  return (
+      <MapContainer center={[35, -100]} zoom={4}scrollWheelZoom={true}>
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+            </MapContainer>
+  );
 }
+
 export default App;
