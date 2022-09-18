4a10d8c05e8a097c1a9c3359b1237c6d8efc4d2c
jeremystocking <68076394+jeremystocking@users.noreply.github.com>
2022-09-17 20:13:00 -0600

    Docker, Django, MySQL all functioning
    Use docker-compose up in f22-fires-wild
    Go to http://localhost:8000/data2019/results/

---
+++ ffp-backend/data2019/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Data2019Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'data2019'
--- ffp-backend/ffp-backend/settings.py
+++ ffp-backend/ffp-backend/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'data2019.apps.Data2019Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'MST'
 
 USE_I18N = True
 
