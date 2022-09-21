8eb7d2a95fd22ee383bb15d373fa3ba04c48a560
Sam W <61138994+samwise-311@users.noreply.github.com>
2022-09-21 14:48:36 -0600

    working on #89 (#91)

--- fire-react-app/src/Navbar.js
+++ fire-react-app/src/Navbar.js
@@ -1,26 +1,31 @@
-import React from "react";
-import "./styles.css";
-import logo from "./components/ffp_logo.png";
+import React from "react"
+import "./styles.css"
+import logo from "./components/ffp_logo.png"
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
-function Navbar() {
+export default function Navbar() {
   return (
     <nav className="nav">
-    <a href="/">
+    <Link to="/">
       <img alt="[LOGO]" className="logo" src={logo} />
-    </a>
+    </Link>
     <ul>
-      <li>
-        <a href="/Data">Data</a>
-      </li>
-      <li>
-        <a href="/About">About</a>
-      </li>
-      <li>
-        <a href="/Admin">Admin</a>
-      </li>
+        <CustomLink to="/Data">Data</CustomLink>
+        <CustomLink to="/About">About</CustomLink>
+        <CustomLink to="/Admin">Admin</CustomLink>
     </ul>
   </nav>
   )
 }
 
-export default Navbar;
+function CustomLink({ to, children, ...props }) {
+ const resolvedPath = useResolvedPath(to)
+ const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+  return (
+    <li className={ isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
