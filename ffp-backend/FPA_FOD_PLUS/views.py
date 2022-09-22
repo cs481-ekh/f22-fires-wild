9ac0f98e60acd54b4a21b022128ec4692446029a
Benjamin Collins <55720135+collins-benjaminw@users.noreply.github.com>
2022-09-22 15:49:54 -0600

    add function for heatmap objects (#102)
    * add function for heatmap objects
    
    * filter by august 2018

--- ffp-backend/FPA_FOD_PLUS/views.py
+++ ffp-backend/FPA_FOD_PLUS/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import HeatMapSerializer
+from django.db.models import Q
+
 
 from .models import Data
 
@@ -6,6 +12,34 @@ def index(request):
     
     return HttpResponse("Hello, world. You're at the FPA-FOD-Plus index page.")
     
+@api_view(['GET'])
+def heat_map(request):
+    if request.method == 'GET':
+        # limit to august fires 
+        data = Data.objects.filter(
+            FIRE_YEAR=2018,
+            DISCOVERY_DOY__lte=243,
+            DISCOVERY_DOY__gte=213
+        # Filter out any broken latitude / longitude fields
+        ).exclude(
+            LATITUDE=0
+        ).exclude(
+            LONGITUDE=0
+        )
+
+        serializer = HeatMapSerializer(data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
+
+    # elif request.method == 'POST':
+    #     serializer = StudentSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(status=status.HTTP_201_CREATED)
+
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 def results(request):
     query_results = Data.objects.filter(NWCG_REPORTING_UNIT_ID='USCACDF')
     response = ""
--- ffp-backend/ffp-backend/settings.py
+++ ffp-backend/ffp-backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ffp-backend.urls'
@@ -128,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# TODO: Should this only be local
+# https://blog.logrocket.com/using-react-django-create-app-tutorial/
+CORS_ORIGIN_ALLOW_ALL = True
