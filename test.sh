bc2ed633116565c9594ccc56bcc9c14c29683789
Benjamin Collins <benjamincollins@u.boisestate.edu>
2022-10-16 14:37:23 -0600

    add a timeout to test in case port isn't open

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+while (! (: </dev/tcp/localhost/8000) &> /dev/null || ! (: </dev/tcp/localhost/3000) &> /dev/null); do
+    sleep 5;
+    "waiting for django (locahost 8000) or react (locahost 3000)"
+done
+
 cd functional-testing
 
 yarn
