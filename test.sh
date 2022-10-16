eba514428f1d7638378a187bc40928490c256405
Benjamin Collins <benjamincollins@u.boisestate.edu>
2022-10-16 14:44:11 -0600

    oops

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -e
 
 while (! (: </dev/tcp/localhost/8000) &> /dev/null || ! (: </dev/tcp/localhost/3000) &> /dev/null); do
     sleep 5;
-    "waiting for django (locahost 8000) or react (locahost 3000)"
+    echo "waiting for django (locahost 8000) or react (locahost 3000)"
 done
 
 cd functional-testing
