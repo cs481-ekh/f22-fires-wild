b6e870e1655a4e5b8eecc7431ea32860fd96479d
Benjamin Collins <benjamincollins@u.boisestate.edu>
2022-10-16 19:16:15 -0600

    use proper ip

--- sample-data-import.sh
+++ sample-data-import.sh
@@ -44,4 +44,8 @@ docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}
 echo "GET localhost:8000 heatmap"
 curl -XGET 'http://localhost:8000/api/heatmap/' -v
 
+echo "GET 172.18.0.3:8000 heatmap"
+curl -XGET 'http://172.18.0.3:8000/api/heatmap/' -v
+
+172.18.0.3
 
