394bce3a9d42148bb420a400370ea514cee32c16
Benjamin Collins <benjamincollins@u.boisestate.edu>
2022-10-16 19:19:19 -0600

    oops

--- sample-data-import.sh
+++ sample-data-import.sh
@@ -41,8 +41,8 @@ docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}
 
 # HACK try curl to the heatmap endpoint
 
-echo "GET localhost:8000 heatmap"
-curl -XGET 'http://localhost:8000/api/heatmap/' -v
+# echo "GET localhost:8000 heatmap"
+# curl -XGET 'http://localhost:8000/api/heatmap/' -v
 
 echo "GET 172.18.0.3:8000 heatmap"
 curl -XGET 'http://172.18.0.3:8000/api/heatmap/' -v
