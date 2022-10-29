a65a73ee9683e41774f48d586982853e8521f078
jeremystocking <68076394+jeremystocking@users.noreply.github.com>
2022-10-29 10:02:36 -0600

    Updated variable list descriptions

--- ffp-backend/FPA_FOD_PLUS/serializers.py
+++ ffp-backend/FPA_FOD_PLUS/serializers.py
@@ -28,20 +28,20 @@ class VariableListSerializer(serializers.Serializer):
         child=serializers.CharField()
     )
     data = { 
-        'FIRE_YEAR' : 'Description',
-        'DISCOVERY_DATE' : 'Description',
-        'DISCOVERY_DOY' : 'Description',
-        'DISCOVERY_TIME' : 'Description',
-        'CONT_DATE' : 'Description',
-        'CONT_DOY' : 'Description',
-        'CONT_TIME' : 'Description',
-        'STATE' : 'Description',
-        'COUNTY' : 'Description',
-        'Ecoregion_US_L4CODE' : 'Description',
-        'Ecoregion_US_L3CODE' : 'Description',
-        'Ecoregion_NA_L3CODE' : 'Description',
-        'Ecoregion_NA_L2CODE' : 'Description',
-        'Ecoregion_NA_L1CODE' : 'Description',
+        'FIRE_YEAR' : 'Calendar year in which the fire was discovered or confirmed to exist',
+        'DISCOVERY_DATE' : 'Date on which the fire was discovered or confirmed to exist',
+        'DISCOVERY_DOY' : 'Day of year on which the fire was discovered or confirmed to exist',
+        'DISCOVERY_TIME' : 'Time of day that the fire was discovered or confirmed to exist',
+        'CONT_DATE' : 'Date on which the fire was declared contained or otherwise controlled (mm/dd/yyyy where mm=month, dd=day, and yyyy=year)',
+        'CONT_DOY' : 'Day of year on which the fire was declared contained or otherwise controlled',
+        'CONT_TIME' : 'Time of day that the fire was declared contained or otherwise controlled (hhmm where hh=hour, mm=minutes)',
+        'STATE' : 'Two-letter alphabetic code for the state in which the fire burned (or originated), based on the nominal designation in the fire report',
+        'COUNTY' : 'County, or equivalent, in which the fire burned (or originated), based on nominal designation in the fire report',
+        'Ecoregion_US_L4CODE' : 'Ecoregion level 4 code in the US',
+        'Ecoregion_US_L3CODE' : 'Ecoregion level 3 code in the US',
+        'Ecoregion_NA_L3CODE' : 'Ecoregion level 3 code in the North America',
+        'Ecoregion_NA_L2CODE' : 'Ecoregion level 2 code in the North America',
+        'Ecoregion_NA_L1CODE' : 'Ecoregion level 1 code in the North America',
     }
         
     def get_variableList():
