926b8e31a64479ac05202160ed0aef4368877b38
jeremystocking <68076394+jeremystocking@users.noreply.github.com>
2022-10-29 10:05:16 -0600

    Test for Descriptions
    Added test for descriptions

--- functional-testing/cypress/e2e/api/variable_list.cy.js
+++ functional-testing/cypress/e2e/api/variable_list.cy.js
@@ -1,34 +1,46 @@
 const url = "localhost:8000/variable_list/";
+	
+    const data = { 
+        'FIRE_YEAR' : 'Calendar year in which the fire was discovered or confirmed to exist',
+        'DISCOVERY_DATE' : 'Date on which the fire was discovered or confirmed to exist',
+        'DISCOVERY_DOY' : 'Day of year on which the fire was discovered or confirmed to exist',
+        'DISCOVERY_TIME' : 'Time of day that the fire was discovered or confirmed to exist',
+        'CONT_DATE' : 'Date on which the fire was declared contained or otherwise controlled (mm/dd/yyyy where mm=month, dd=day, and yyyy=year)',
+        'CONT_DOY' : 'Day of year on which the fire was declared contained or otherwise controlled',
+        'CONT_TIME' : 'Time of day that the fire was declared contained or otherwise controlled (hhmm where hh=hour, mm=minutes)',
+        'STATE' : 'Two-letter alphabetic code for the state in which the fire burned (or originated), based on the nominal designation in the fire report',
+        'COUNTY' : 'County, or equivalent, in which the fire burned (or originated), based on nominal designation in the fire report',
+        'Ecoregion_US_L4CODE' : 'Ecoregion level 4 code in the US',
+        'Ecoregion_US_L3CODE' : 'Ecoregion level 3 code in the US',
+        'Ecoregion_NA_L3CODE' : 'Ecoregion level 3 code in the North America',
+        'Ecoregion_NA_L2CODE' : 'Ecoregion level 2 code in the North America',
+        'Ecoregion_NA_L1CODE' : 'Ecoregion level 1 code in the North America',
+    };
 
-const variables = [
-	"FIRE_YEAR",
-	"DISCOVERY_DATE",
-	"DISCOVERY_DOY",
-	"DISCOVERY_TIME",
-	"CONT_DATE",
-	"CONT_DOY",
-	"CONT_TIME",
-	"STATE",
-	"COUNTY",
-	"Ecoregion_US_L4CODE",
-	"Ecoregion_US_L3CODE",
-	"Ecoregion_NA_L3CODE",
-	"Ecoregion_NA_L2CODE",
-	"Ecoregion_NA_L1CODE"
-	];
 
 describe("variable_list.cy.js", () => {
 	it("should be visitable", () => {
 		cy.visit(url);
 	})
-	it("serializer should match list given", () => {
+	it("serializer key should match list given", () => {
 		cy.request({
 			method: "GET",
 			url,
 		}).then((response) => {
 			let variable_list = response.body;
 			for(const [key, value] of Object.entries(variable_list)){
-				expect(key).to.be.oneOf(variables);
+				expect(key).to.be.oneOf(Object.keys(data));
+			}
+		})
+	})
+	it("serializer descriptions should match list given", () => {
+		cy.request({
+			method: "GET",
+			url,
+		}).then((response) => {
+			let variable_list = response.body;
+			for(const [key, value] of Object.entries(variable_list)){
+				expect(value).to.be.oneOf(Object.values(data));
 			}
 		})
 	})
