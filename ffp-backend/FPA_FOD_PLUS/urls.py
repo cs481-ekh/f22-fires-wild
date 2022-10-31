ccfba37a4213e81d24df0507cd03f8f4820c8f71
Benjamin Collins <55720135+collins-benjaminw@users.noreply.github.com>
2022-10-30 18:38:58 -0600

    229 fix django routing (#231)
    * try to look at instance to find structure
    
    * fix django routing
    
    * fix tests
    
    * oops
    
    * fix
    
    * oops

--- ffp-backend/FPA_FOD_PLUS/urls.py
+++ ffp-backend/FPA_FOD_PLUS/urls.py
@@ -3,9 +3,4 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    
-    path('results/', views.results, name='results'),
-    
-    path('administrator/', views.administrator, name='administrator'),
 ]
