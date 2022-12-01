f3f60294a30f979c5305e21e9424372ee9cd0e40
Benjamin Collins <benjamincollins@u.boisestate.edu>
2022-12-01 13:35:14 -0700

    possible fix on settings

--- ffp-backend/ffp-backend/settings.py
+++ ffp-backend/ffp-backend/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
